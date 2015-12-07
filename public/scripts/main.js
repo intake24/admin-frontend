fc111e50c91ae445b2499f1ace9843221dbbd4fe
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2015-12-07 14:14:51 +0000

    Added flags to language selector

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -81,7 +81,7 @@ app.filter('serving-image-set-filter', function () {
 
 app.factory('SharedData', function () {
     return {
-      locales: [{'language':gettext('Arabic'), 'locale':'ar', 'intake_locale':'ar_AE', 'changed':false}, {'language':gettext('English'), 'locale':'en', 'intake_locale':'en_GB','changed':false}],
+      locales: [{'language':gettext('Arabic'), 'locale_language':'العربية', 'locale':'ar', 'intake_locale':'ar_AE', 'flag':'ar', 'changed':false}, {'language':gettext('English'), 'locale_language':'English', 'locale':'en', 'intake_locale':'en_GB', 'flag':'england','changed':false}],
       locale: {'language':gettext('English'), 'locale':'en', 'intake_locale':'en_GB', 'changed':false},
       estimationMethods: [{'name':gettext('As served'), 'slug':'as-served'}, {'name':gettext('Guide Image'), 'slug':'guide-image'}, {'name':gettext('Drink scale'), 'slug':'drink-scale'}, {'name':gettext('Standard portion'), 'slug':'standard-portion'}, {'name':gettext('Cereal'), 'slug':'cereal'}, {'name':gettext('Milk on cereal'), 'slug':'milk-on-cereal'}, {'name':gettext('Milk in a hot drink'), 'slug':'milk-in-a-hot-drink'}, {'name':gettext('Pizza'), 'slug':'pizza'}],
       cerealTypes: [{'name':gettext('Hoop'), 'slug':'hoop'}, {'name':gettext('Flake'), 'slug':'flake'}, {'name':gettext('Rice krispie'), 'slug':'rice-krispie'}],

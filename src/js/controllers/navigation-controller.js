cfb06a42940cde83bbd50bd01a574f21f3e8b07d
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2016-07-02 17:59:05 +0100

    removed jQuery from navigation-controller

--- src/js/controllers/navigation-controller.js
+++ src/js/controllers/navigation-controller.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var $ = require('jquery');
-
 module.exports = function(app) {
     app.controller('NavigationController', ["$rootScope", "$scope", "Locales", "CurrentItem", controllerFun]);
 };
@@ -22,73 +20,34 @@ function controllerFun($rootScope, $scope, locales, currentItem) {
 
     $scope.currentItem = currentItem;
 
-    /*if (locale) {
-     showMessage(format('Language set to %s', [locale.language]), 'success');
-
-     SharedData.locale = locale;
-     $cookies.putObject("locale", locale);
-     }*/
-    ;
-
     $scope.setLocale = function (intake24_locale, ui_locale) {
         document.location = '/' + intake24_locale + '/' + ui_locale;
-    }
-
-    $('.sidebar-btn').click(function () {
-        if ($(this).closest("li").children("ul").length == 0) {
-            $('.sidebar').removeClass('active');
-        }
-    });
-
-    function showContainer(container_id) {
-        $('.properties-container:not(' + container_id + ')').hide();
-        $(container_id).show();
-    }
-
-
-    /******************/
-    /** Manage foods **/
-    /******************/
-
-    // Browse foods
-    $('#browse-foods-btn').click(function () {
-
-        $('#properties-col').removeClass('fullwidth');
-        $('#add-new-food-container').hide();
-        $('#add-new-category-container').hide();
-
-        // fetchCategoriesService.broadcast();
-
-        $(".food-list-container").show();
-        $('#food-list-col').show().animate({'opacity': 1}, function () {
-            $('#properties-col').addClass('active');
-        });
-    });
+    };
 
     $scope.addNewFood = function () {
         $rootScope.$broadcast('intake24.admin.food_db.AddNewFood');
-    }
+    };
 
     $scope.addNewCategory = function () {
         $rootScope.$broadcast('intake24.admin.food_db.AddNewCategory');
-    }
+    };
 
     $scope.cloneFoodEnabled = function () {
         var curItem = $scope.currentItem.getCurrentItem();
         return (curItem && curItem.type == 'food');
-    }
+    };
 
     $scope.cloneFood = function () {
         $rootScope.$broadcast('intake24.admin.food_db.CloneFood');
-    }
+    };
 
     $scope.deleteItemEnabled = function () {
         return $scope.currentItem.getCurrentItem();
-    }
+    };
 
     $scope.deleteItem = function () {
         if ($scope.deleteItemEnabled())
             $scope.currentItem.delete();
-    }
+    };
 
 }

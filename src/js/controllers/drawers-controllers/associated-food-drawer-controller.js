7069316ed25b3fdf80958796bf0a6e36b92bbd12
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2016-08-03 12:08:29 +0100

    Fix #40: prevent search watch from triggering on page load

--- src/js/controllers/drawers-controllers/associated-food-drawer-controller.js
+++ src/js/controllers/drawers-controllers/associated-food-drawer-controller.js
@@ -11,10 +11,16 @@ function controllerFun($scope, FoodDataReaderService, DrawersService) {
     $scope.items = [];
     $scope.isOpen = DrawersService.drawerAssociatedFood.getOpen();
 
-    $scope.$watch('query', function () {
+    $scope.$watch('query', function (newValue, oldValue) {
+      $scope.items.length = 0;
+
+      // #40: this triggering on load causes an error, the correct way to prevent
+      // the watch from triggering on load seems to be checking if newValue == oldValue,
+      // but in this case an empty query should not be sent to the server anyway
+
+      if (query) {
         FoodDataReaderService.searchFoods($scope.query).then(function (data) {
-            $scope.items.length = 0;
-            Array.prototype.push.apply($scope.items, data);
+          Array.prototype.push.apply($scope.items, data);
         });
     });
 

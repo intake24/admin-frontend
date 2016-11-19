f18568a7f9a24053c6139c52868daa6a1abba96c
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2016-11-19 12:12:42 +0000

    removed console.log

--- src/js/services/food-data-writer-service.js
+++ src/js/services/food-data-writer-service.js
@@ -24,7 +24,6 @@ function serviceFun($http, locales) {
         },
 
         updateCategoryMainRecord: function (category_code, definition) {
-            console.log(definition);
             return $http.post(api_base_url + 'admin/categories/' + category_code, definition);
         },
 

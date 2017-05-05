769f9ab99a5255511008fd6b45eed39c7dd939b5
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2017-05-05 15:36:51 +0100

    image service fix

--- src/js/image-gallery/services/image-service.js
+++ src/js/image-gallery/services/image-service.js
@@ -7,15 +7,16 @@
 var sendImageFile = require("./send-image-file");
 
 module.exports = function (app) {
-    app.service("ImageService", ["$http", "$httpParamSerializerJQLike", "$q", "$timeout", "HttpRequestInterceptor",
+    app.service("ImageService", ["$http", "$httpParamSerializerJQLike", "$q", "$window",
+        "HttpRequestInterceptor",
         serviceFun]);
 };
 
-function serviceFun($http, $httpParamSerializerJQLike, $q, $timeout, HttpRequestInterceptor) {
+function serviceFun($http, $httpParamSerializerJQLike, $q, $window, HttpRequestInterceptor) {
 
     return {
         query: function (offset, limit, search) {
-            var url = "http://api-test.intake24.co.uk/admin/images/source",
+            var url = $window.api_base_url + "admin/images/source",
                 params = {offset: offset, limit: limit};
             if (search) {
                 params.search = search;
@@ -23,19 +24,19 @@ function serviceFun($http, $httpParamSerializerJQLike, $q, $timeout, HttpRequest
             return $http.get(url + "?" + $httpParamSerializerJQLike(params))
         },
         add: function (file) {
-            var url = "http://api-test.intake24.co.uk/admin/images/source/new";
+            var url = $window.api_base_url + "admin/images/source/new";
             return sendImageFile(url, file, HttpRequestInterceptor, $q);
         },
         addForAsServed: function (asServedSetId, file) {
-            var url = "http://api-test.intake24.co.uk/admin/images/source/new-as-served?setId=" + asServedSetId;
+            var url = $window.api_base_url + "admin/images/source/new-as-served?setId=" + asServedSetId;
             return sendImageFile(url, file, HttpRequestInterceptor, $q);
         },
         patch: function (id, tags) {
-            var url = "http://api-test.intake24.co.uk/admin/images/source/" + id;
+            var url = $window.api_base_url + "admin/images/source/" + id;
             return $http.patch(url, {keywords: tags});
         },
         remove: function (ids) {
-            var url = "http://api-test.intake24.co.uk/admin/images/source/delete";
+            var url = $window.api_base_url + "admin/images/source/delete";
             return $http({
                 method: "DELETE",
                 url: url,

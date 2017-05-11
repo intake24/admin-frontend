6ca5a76c27399a931b0881d7f64e58082c021b65
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2017-05-11 20:05:46 +0100

    resolve #160

--- src/js/explorer/controllers/search-controller.js
+++ src/js/explorer/controllers/search-controller.js
@@ -16,6 +16,7 @@ function controllerFun($scope, $rootScope, $routeParams, $timeout, LocalesServic
     $scope.searchResultsAreVisible = false;
     $scope.query = "";
     $scope.focused = false;
+    $scope.queryTextDirection = "";
 
     $scope.getActive = function () {
         return this.query != '' || this.focused;
@@ -29,11 +30,23 @@ function controllerFun($scope, $rootScope, $routeParams, $timeout, LocalesServic
         $rootScope.$broadcast('intake24.admin.food_db.AddNewCategory');
     };
 
+    $scope.getFoodTextDirection = function (food) {
+        if (food.localDescription && food.localDescription.defined) {
+            return $scope.queryTextDirection;
+        }
+    };
+
     $scope.$watch('query', function (oldValue, newValue) {
         if (newValue) {
             $timeout.cancel(timeoutPromise);
             timeoutPromise = $timeout(performFoodSearch, queryTimeout);
         }
+    });
+
+    $scope.$watch(function () {
+        return $scope.$parent.getTextDirection();
+    }, function (newVal) {
+        $scope.queryTextDirection = newVal;
     });
 
     function performFoodSearch() {

09dcc124259e38efcb394c5725dfd0ce332b3099
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2017-05-19 15:46:05 +0100

    ie string startswith

--- src/js/polyfills/string/ends-with.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-module.exports = function () {
-    String.prototype.endsWith = function (suffix) {
-        return this.indexOf(suffix, this.length - suffix.length) !== -1;
-    };
-};

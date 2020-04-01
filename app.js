a682d15fa85f3dc60e31216c805467d3a75848ed
Lukas Hroch <lukas.hroch@mrc-epid.cam.ac.uk>
2020-04-01 12:55:19 +0100

    initial content security policy

--- app.js
+++ app.js
@@ -6,25 +6,59 @@ var cors = require('cors');
 var helmet = require('helmet');
 var i18n = require('i18n-abide');
 var logger = require('morgan');
+var uuid = require('uuid');
 var config = require('./config');
 var routes = require('./routes');
 
 var app = express();
-app.use(helmet(config.helmet));
 
-app.use(i18n.abide({
-  supported_languages: ['ar', 'en'],
-  translation_directory: 'public/i18n',
-  template_engine: 'pug',
-  template_file_ext: 'pug'
-}));
+// Initialize i18n first
+// i18n overrides res.locals, it doesn't merge it with current res.locals object
+app.use(
+  i18n.abide({
+    supported_languages: ['ar', 'en'],
+    translation_directory: 'public/i18n',
+    template_engine: 'pug',
+    template_file_ext: 'pug'
+  })
+);
+
+// Generate NONCE for CSP
+app.use((req, res, next) => {
+  res.locals.nonce = uuid.v4();
+  next();
+});
+
+// Initialize Helmet
+app.use(
+  helmet(
+    {
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: ["'self'", (req, res) => `'nonce-${res.locals.nonce}'`],
+          imgSrc: ["'self'", 'blob:', config.apiBaseUrl],
+          styleSrc: [
+            "'self'",
+            // TODO: fix AngularJS inline style
+            "'unsafe-inline'",
+          ],
+          connectSrc: ["'self'", config.apiBaseUrl]
+        }
+      },
+      ...config.helmet
+    },
+  )
+);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
-app.use(cors({
+app.use(
+  cors({
     allowedOrigins: ['*'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']
-}));
+  })
+);
 
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));

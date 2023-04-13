f6071b91490ab46c01305d583396533974ea9a86
Ivan Poliakov <ivpolyakov@gmail.com>
2023-04-13 18:30:31 +0100

    (feat)(wip) drink scale image uploader

---
+++ src/js/image-gallery/services/drinkware-service.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var _ = require("underscore");
+const angular = require("angular");
+
+module.exports = function (app) {
+    app.service("DrinkwareService", ["$http", "$window", function ($http, $window) {
+
+        const BASE_URL = $window.api_base_url + "admin/portion-size/drinkware";
+
+        return {
+            upload: function (id, setImageFile, setOutlinesFile, volumeSamplesFile, slidingScales) {
+                let fd = new FormData();
+                fd.append("setId", id);
+                fd.append("setImageFile", setImageFile);
+                fd.append("setOutlinesFile", setOutlinesFile);
+                fd.append("volumeSamplesFile", volumeSamplesFile);
+
+                fd.append("scaleCount", slidingScales.length);
+
+                slidingScales.forEach((scale, index) => {
+                    fd.append(`scaleObjectId[${index}]`, scale.objectId);
+                    fd.append(`scaleImageFile[${index}]`, scale.baseImage);
+                    fd.append(`scaleOutlineFile[${index}]`, scale.outline);
+                });
+
+                return $http.post(BASE_URL + "/upload", fd, {
+                    transformRequest: angular.identity,
+                    headers: {"Content-Type": undefined}
+                });
+            },
+        }
+    }]);
+}
